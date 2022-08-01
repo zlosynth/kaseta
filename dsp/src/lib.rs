2c58c615f62d8b8d480435b9d3de003e27250156
Petr Horacek <hrck@protonmail.com>
2022-08-01 22:04:14 +0200

    Implement smoothed value signal
    This will be used to smoothen parameters set through the control loop.
    
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- dsp/src/lib.rs
+++ dsp/src/lib.rs
@@ -1,4 +1,9 @@
 //! Digital signal processing components that must run in real-time.
 
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
+
 pub mod hysteresis;
 pub mod processor;
+pub mod smoothed_value;
