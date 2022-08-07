9352aab8d31684f2725a5a0a574157b4dbedfff4
Petr Horacek <hrck@protonmail.com>
2022-08-07 21:21:15 +0200

    Add a bench for oversampling
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- dsp/benches/oversampling.rs
+++ dsp/benches/oversampling.rs
@@ -1,8 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("hysteresis", |b| {
+    c.bench_function("oversampling", |b| {
         use kaseta_dsp::hysteresis::{Hysteresis, SignalApplyHysteresis};
+        use kaseta_dsp::oversampling::{
+            Downsampler8, SignalDownsample, SignalUpsample, Upsampler8,
+        };
         use sirena::signal::{self, Signal, SignalTake};
 
         const BUFFER_SIZE: usize = 32;
@@ -10,6 +13,9 @@ fn criterion_benchmark(c: &mut Criterion) {
         const FS: f32 = 48_000.0;
         const FREQ: f32 = 100.0;
         let mut input = signal::sine(FS, FREQ);
+
+        let mut upsampler = Upsampler8::new_8();
+        let mut downsampler = Downsampler8::new_8();
 
         const DRIVE: f32 = 0.5;
         const SATURATION: f32 = 0.5;
@@ -22,12 +28,8 @@ fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| {
             let _buffer: [f32; BUFFER_SIZE] = input
                 .by_ref()
-                .apply_hysteresis(
-                    &mut hysteresis,
-                    drive.by_ref(),
-                    saturation.by_ref(),
-                    width.by_ref(),
-                )
+                .upsample(&mut upsampler)
+                .downsample(&mut downsampler)
                 .take(BUFFER_SIZE)
                 .collect::<Vec<_>>()
                 .as_slice()
