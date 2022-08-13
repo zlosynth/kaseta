08dac2d9b39538ae273b43a14bdf223968f0036e
Petr Horacek <hrck@protonmail.com>
2022-08-13 21:01:50 +0200

    Initialize control package
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

---
+++ control/src/lib.rs
@@ -0,0 +1,14 @@
+//! Components of user inteface, passing user input to DSP.
+
+#![allow(clippy::items_after_statements)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_lossless)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::cast_possible_wrap)]
+#![allow(clippy::cast_precision_loss)]
+
+#[must_use]
+pub fn foo() -> u32 {
+    1
+}
