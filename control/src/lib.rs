249a4804833e51292abe08d2c2f9d04c3cd6f3e5
Petr Horacek <hrck@protonmail.com>
2022-08-14 13:03:31 +0200

    Introduce a control interface
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- control/src/lib.rs
+++ control/src/lib.rs
@@ -1,4 +1,27 @@
-//! Components of user inteface, passing user input to DSP.
+//! Components of user inteface, passing user input to DSP and reactions back.
+//!
+//! It is mainly targetted to run in a firmware with multiple loops running in
+//! different frequencies, passing messages from one to another. However, parts
+//! of it may be useful in software as well.
+//!
+//! Following is an example of communication that may be used in an eurorack
+//! module:
+//!
+//! ```text
+//!                    [ DSPLoop ]
+//!                       A   |
+//!           (DSPAction) |   | (DSPAction)
+//!                       |   V
+//!                 [ Reducer {Cache} ] <--------> {Store}
+//!                     A        |
+//!     (ControlAction) |        | (DisplayReaction)
+//!             +-------+        +--+
+//!             |                   |
+//!             |                   V
+//!    [ ControlLoop ]         [ DisplayLoop ]
+//!     |     |     |                 |
+//!   [CV] [Pots] [Buttons]         [LEDs]
+//! ```
 
 #![allow(clippy::items_after_statements)]
 #![allow(clippy::module_name_repetitions)]
@@ -8,7 +31,127 @@
 #![allow(clippy::cast_possible_wrap)]
 #![allow(clippy::cast_precision_loss)]
 
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
+
+use kaseta_dsp::processor::Attributes;
+
+mod taper;
+
+// 0.1 is almost clean, and with high pre-amp it still passes through signal.
+// 30 is well in the extreme, but still somehow stable.
+const DRIVE_RANGE: (f32, f32) = (0.1, 30.0);
+
+const SATURATION_RANGE: (f32, f32) = (0.0, 1.0);
+
+// Width (1.0 - bias) must never reach 1.0, otherwise it panics due to division by zero
+const BIAS_RANGE: (f32, f32) = (0.0001, 1.0);
+
+#[derive(Clone, Copy, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum ControlAction {
+    SetDrivePot(f32),
+    SetDriveCV(f32),
+    SetSaturationPot(f32),
+    SetSaturationCV(f32),
+    SetBiasPot(f32),
+    SetBiasCV(f32),
+}
+
+#[derive(Clone, Copy, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct DSPReaction {
+    pub drive: f32,
+    pub saturation: f32,
+    pub bias: f32,
+}
+
+impl From<DSPReaction> for Attributes {
+    fn from(other: DSPReaction) -> Self {
+        Attributes {
+            drive: other.drive,
+            saturation: other.saturation,
+            width: 1.0 - other.bias,
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct Cache {
+    pub drive_pot: f32,
+    pub drive_cv: f32,
+    pub saturation_pot: f32,
+    pub saturation_cv: f32,
+    pub bias_pot: f32,
+    pub bias_cv: f32,
+}
+
 #[must_use]
-pub fn foo() -> u32 {
-    1
+pub fn reduce_control_action(action: ControlAction, cache: &mut Cache) -> DSPReaction {
+    apply_control_action_in_cache(action, cache);
+    cook_dsp_reaction_from_cache(cache)
+}
+
+#[must_use]
+pub fn cook_dsp_reaction_from_cache(cache: &Cache) -> DSPReaction {
+    let drive = calculate_drive(cache);
+    let saturation = calculate_saturation(cache);
+    let bias = calculate_bias(cache);
+    DSPReaction {
+        drive,
+        saturation,
+        bias,
+    }
+}
+
+#[allow(clippy::let_and_return)]
+fn calculate_drive(cache: &Cache) -> f32 {
+    let drive_sum = (cache.drive_pot + cache.drive_cv).clamp(0.0, 1.0);
+    let drive_curved = taper::log(drive_sum);
+    let drive_scaled = drive_curved * (DRIVE_RANGE.1 - DRIVE_RANGE.0) + DRIVE_RANGE.0;
+    drive_scaled
+}
+
+#[allow(clippy::let_and_return)]
+fn calculate_saturation(cache: &Cache) -> f32 {
+    let saturation_sum = (cache.saturation_pot + cache.saturation_cv).clamp(0.0, 1.0);
+    let saturation_curved = taper::reverse_log(saturation_sum);
+    let saturation_scaled =
+        saturation_curved * (SATURATION_RANGE.1 - SATURATION_RANGE.0) + SATURATION_RANGE.0;
+    saturation_scaled
+}
+
+#[allow(clippy::let_and_return)]
+fn calculate_bias(cache: &Cache) -> f32 {
+    let bias_sum = (cache.bias_pot + cache.bias_cv).clamp(0.0, 1.0);
+    let bias_curved = taper::log(bias_sum);
+    let bias_scaled = bias_curved * (BIAS_RANGE.1 - BIAS_RANGE.0) + BIAS_RANGE.0;
+    bias_scaled
+}
+
+fn apply_control_action_in_cache(action: ControlAction, cache: &mut Cache) {
+    #[allow(clippy::enum_glob_use)]
+    use ControlAction::*;
+    match action {
+        SetDrivePot(x) => {
+            cache.drive_pot = x;
+        }
+        SetDriveCV(x) => {
+            cache.drive_cv = x;
+        }
+        SetSaturationPot(x) => {
+            cache.saturation_pot = x;
+        }
+        SetSaturationCV(x) => {
+            cache.saturation_cv = x;
+        }
+        SetBiasPot(x) => {
+            cache.bias_pot = x;
+        }
+        SetBiasCV(x) => {
+            cache.bias_cv = x;
+        }
+    }
 }
---
+++ control/src/taper.rs
@@ -0,0 +1,97 @@
+const LOG: [f32; 22] = [
+    0.0,
+    0.005,
+    0.019_996_643,
+    0.040_958_643,
+    0.062_983_93,
+    0.086_186_11,
+    0.110_698_28,
+    0.136_677_15,
+    0.164_309_44,
+    0.19382,
+    0.225_483,
+    0.259_637_3,
+    0.296_708_64,
+    0.337_242_2,
+    0.381_951_87,
+    0.431_798_22,
+    0.488_116_62,
+    0.552_841_96,
+    0.628_932_1,
+    0.721_246_36,
+    0.838_632,
+    1.0,
+];
+
+pub fn log(position: f32) -> f32 {
+    if position < 0.0 {
+        return 0.0;
+    } else if position > 1.0 {
+        return 1.0;
+    }
+
+    let array_position = position * (LOG.len() - 1) as f32;
+    let index_a = array_position as usize;
+    let index_b = (array_position as usize + 1).min(LOG.len() - 1);
+    let remainder = array_position.fract();
+
+    let value = LOG[index_a];
+    let delta_to_next = LOG[index_b] - LOG[index_a];
+
+    value + delta_to_next * remainder
+}
+
+pub fn reverse_log(position: f32) -> f32 {
+    1.0 - log(1.0 - position)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn log_taper_below_zero() {
+        assert_relative_eq!(log(-1.0), 0.0);
+    }
+
+    #[test]
+    fn log_taper_above_one() {
+        assert_relative_eq!(log(2.0), 1.0);
+    }
+
+    #[test]
+    fn log_taper_within_limits() {
+        assert_relative_eq!(log(0.0), 0.0);
+        assert_relative_eq!(log(0.025), 0.002_625_000_2);
+        assert_relative_eq!(log(0.05), 0.005_749_833_3);
+        assert_relative_eq!(log(0.1), 0.022_092_845);
+        assert_relative_eq!(log(0.3), 0.118_491_95);
+        assert_relative_eq!(log(0.5), 0.242_560_15);
+        assert_relative_eq!(log(0.7), 0.416_844_3);
+        assert_relative_eq!(log(0.9), 0.712_014_9);
+        assert_relative_eq!(log(1.0), 1.0);
+    }
+
+    #[test]
+    fn reverse_log_taper_below_zero() {
+        assert_relative_eq!(reverse_log(-1.0), 0.0);
+    }
+
+    #[test]
+    fn reverse_log_taper_above_one() {
+        assert_relative_eq!(reverse_log(2.0), 1.0);
+    }
+
+    #[test]
+    fn reverse_log_taper_within_limits() {
+        assert_relative_eq!(reverse_log(0.0), 0.0);
+        assert_relative_eq!(reverse_log(0.1), 0.287_985_1);
+        assert_relative_eq!(reverse_log(0.3), 0.583_155_7);
+        assert_relative_eq!(reverse_log(0.5), 0.757_439_85);
+        assert_relative_eq!(reverse_log(0.7), 0.881_508_05);
+        assert_relative_eq!(reverse_log(0.9), 0.977_907_1);
+        assert_relative_eq!(reverse_log(0.95), 0.994_250_2);
+        assert_relative_eq!(reverse_log(0.975), 0.997_375);
+        assert_relative_eq!(reverse_log(1.0), 1.0);
+    }
+}
