da95a0d5d880952e1e1b0259236b095a31f09f80
Petr Horacek <hrck@protonmail.com>
2022-09-11 16:21:19 +0200

    Improve oversampling performance with cache
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- benches/src/bin/oversampling.rs
+++ benches/src/bin/oversampling.rs
@@ -1,9 +1,14 @@
 //! Oversampling benchmark.
 //!
-//! Measuring how many DWT cycles it takes for X random samples to be upsampled
-//! by 4 and then downsampled.
+//! Measuring how many DWT cycles it takes for a buffer of 32 random samples to
+//! be upsampled by 4 and then downsampled.
 //!
-//! * Original signal-based implementation:
+//! * Original signal-based implementation: 11936078
+//! * Enable icache: 3760126
+//! * Enable dcache: 3648398
+//!
+//! TODO: Unroll loops
+//! TODO: Check more in CMSIS
 
 #![no_main]
 #![no_std]
@@ -14,6 +19,11 @@ use daisy::pac::{DWT};
 use daisy::hal::prelude::_stm32h7xx_hal_rng_RngCore;
 use daisy::hal::prelude::_stm32h7xx_hal_rng_RngExt;
 use daisy::hal::rng::Rng;
+
+use kaseta_dsp::oversampling::{
+    Downsampler4, SignalDownsample, SignalUpsample, Upsampler4,
+};
+use sirena::signal::{self, Signal};
 
 macro_rules! op_cyccnt_diff {
     ( $cp:expr, $x:expr ) => {
@@ -53,19 +63,36 @@ fn random_buffer(randomizer: &mut Rng) -> [f32; 32] {
 fn main() -> ! {
     defmt::println!("Oversampling benchmark");
 
+    const BUFFER_SIZE: usize = 32;
+
     let mut cp = cortex_m::Peripherals::take().unwrap();
     let dp = daisy::pac::Peripherals::take().unwrap();
     let board = daisy::Board::take().unwrap();
     let ccdr = daisy::board_freeze_clocks!(board, dp);
+
+    cp.SCB.enable_icache();
+    cp.SCB.enable_dcache(&mut cp.CPUID);
+
     let mut randomizer = dp.RNG.constrain(ccdr.peripheral.RNG, &ccdr.clocks);
+    let mut upsampler = Upsampler4::new_4();
+    let mut downsampler = Downsampler4::new_4();
 
     let cycles = op_cyccnt_diff!(cp, {
-        for _ in 0..48000 / 32 {
-            let _buffer = random_buffer(&mut randomizer);
+        for _ in 0..300 {
+            let input: [f32; BUFFER_SIZE] = random_buffer(&mut randomizer);
+            let mut output = [0.0; BUFFER_SIZE];
+
+            let mut processed_signal = signal::from_iter(input)
+                .upsample(&mut upsampler)
+                .downsample(&mut downsampler);
+
+            for x in output.iter_mut() {
+                *x = processed_signal.next();
+            }
         }
     });
 
-    defmt::println!("Cycles: {}", cycles);
+    defmt::println!("Cycles per buffer: {}", cycles / 300);
 
     kaseta_benches::exit()
 }
