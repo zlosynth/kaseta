cb19b0339d462fcacd356e332eabf4896a00d7cb
Petr Horacek <hrck@protonmail.com>
2022-09-26 18:39:44 +0200

    Remove offset in position quantization
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- control/src/quantization.rs
+++ control/src/quantization.rs
@@ -28,16 +28,15 @@ pub fn quantize(x: f32, quantization: Quantization) -> f32 {
     match quantization {
         Quantization::Six => {
             const STEP: f32 = 1.0 / 6.0;
-            const HALF: f32 = STEP / 2.0;
-            if x < STEP - HALF {
+            if x < STEP {
                 0.0 * STEP
-            } else if x < 2.0 * STEP - HALF {
+            } else if x < 2.0 * STEP {
                 1.0 * STEP
-            } else if x < 3.0 * STEP - HALF {
+            } else if x < 3.0 * STEP {
                 2.0 * STEP
-            } else if x < 4.0 * STEP - HALF {
+            } else if x < 4.0 * STEP {
                 3.0 * STEP
-            } else if x < 5.0 * STEP - HALF {
+            } else if x < 5.0 * STEP {
                 4.0 * STEP
             } else {
                 5.0 * STEP
@@ -45,20 +44,19 @@ pub fn quantize(x: f32, quantization: Quantization) -> f32 {
         }
         Quantization::Eight => {
             const STEP: f32 = 1.0 / 8.0;
-            const HALF: f32 = STEP / 2.0;
-            if x < STEP - HALF {
+            if x < STEP {
                 0.0 * STEP
-            } else if x < 2.0 * STEP - HALF {
+            } else if x < 2.0 * STEP {
                 1.0 * STEP
-            } else if x < 3.0 * STEP - HALF {
+            } else if x < 3.0 * STEP {
                 2.0 * STEP
-            } else if x < 4.0 * STEP - HALF {
+            } else if x < 4.0 * STEP {
                 3.0 * STEP
-            } else if x < 5.0 * STEP - HALF {
+            } else if x < 5.0 * STEP {
                 4.0 * STEP
-            } else if x < 6.0 * STEP - HALF {
+            } else if x < 6.0 * STEP {
                 5.0 * STEP
-            } else if x < 7.0 * STEP - HALF {
+            } else if x < 7.0 * STEP {
                 6.0 * STEP
             } else {
                 7.0 * STEP
@@ -66,30 +64,29 @@ pub fn quantize(x: f32, quantization: Quantization) -> f32 {
         }
         Quantization::Both => {
             const STEP: f32 = 1.0 / 12.0;
-            const HALF: f32 = STEP / 2.0;
             const EIGHTH: f32 = 1.0 / 8.0;
             const SIXTH: f32 = 1.0 / 6.0;
-            if x < STEP - HALF {
+            if x < STEP {
                 0.0
-            } else if x < 2.0 * STEP - HALF {
+            } else if x < 2.0 * STEP {
                 1.0 * EIGHTH
-            } else if x < 3.0 * STEP - HALF {
+            } else if x < 3.0 * STEP {
                 1.0 * SIXTH
-            } else if x < 4.0 * STEP - HALF {
+            } else if x < 4.0 * STEP {
                 2.0 * EIGHTH
-            } else if x < 5.0 * STEP - HALF {
+            } else if x < 5.0 * STEP {
                 2.0 * SIXTH
-            } else if x < 6.0 * STEP - HALF {
+            } else if x < 6.0 * STEP {
                 3.0 * EIGHTH
-            } else if x < 7.0 * STEP - HALF {
+            } else if x < 7.0 * STEP {
                 1.0 / 2.0
-            } else if x < 8.0 * STEP - HALF {
+            } else if x < 8.0 * STEP {
                 5.0 * EIGHTH
-            } else if x < 9.0 * STEP - HALF {
+            } else if x < 9.0 * STEP {
                 4.0 * SIXTH
-            } else if x < 10.0 * STEP - HALF {
+            } else if x < 10.0 * STEP {
                 6.0 * EIGHTH
-            } else if x < 11.0 * STEP - HALF {
+            } else if x < 11.0 * STEP {
                 5.0 * SIXTH
             } else {
                 7.0 * EIGHTH
