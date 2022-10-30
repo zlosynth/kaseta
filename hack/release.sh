80771b951660a87d8130dac4a823574d367504fb
Petr Horacek <hrck@protonmail.com>
2022-10-30 11:09:32 +0100

    Add release script
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

---
+++ hack/release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+version=${1}
+
+sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
+sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" control/Cargo.toml
+sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" dsp/Cargo.toml
+sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" benches/Cargo.toml
+sed -i "s/rev .*/rev \"v${version}\")/" hardware/Module.kicad_sch
+sed -i "s/gr_text \"board .*\" /gr_text \"board v${version}\" /" hardware/Module.kicad_pcb
+sed -i "s/rev .*/rev \"v${version}\")/" hardware/Module.kicad_pcb
+
+makers
+
+rm -rf release
+mkdir release
+
+export CHANGES=$(awk "/## ${version}/{flag=1;next}/## */{flag=0}flag" CHANGELOG.md | awk 'NF')
+
+envsubst < hack/release.tmpl.md > release/notes.md
