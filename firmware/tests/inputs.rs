7a64dd58be07f1003d01f1837d110c6d8102a5db
Petr Horacek <hrck@protonmail.com>
2022-12-19 14:42:45 +0100

    Improve firmware pot test
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- firmware/tests/inputs.rs
+++ firmware/tests/inputs.rs
@@ -18,77 +18,78 @@ mod tests {
         System::init(cp, dp).inputs
     }
 
-    // #[test]
-    // fn pots_move_in_expected_range(inputs: &mut Inputs) {
-    //     macro_rules! assert_pot_is_up {
-    //         ($name:expr, $pot:expr) => {
-    //             defmt::info!("Turn {} all the way up, then click the button", $name);
-    //             sample_until_button_is_clicked(inputs);
-    //             defmt::assert!($pot > 0.99, "Assert failed, actual value: {:?}", $pot);
-    //             defmt::info!("OK");
-    //         };
-    //     }
+    #[test]
+    fn pots_move_in_expected_range(inputs: &mut Inputs) {
+        macro_rules! assert_pot_is_up {
+            ($name:expr, $pot:expr) => {
+                defmt::info!("Turn {} all the way up, then click the button", $name);
+                sample_until_button_is_clicked(inputs);
+                cortex_m::asm::delay(480_000_000 / 2); // Protection against accidental double-clicks
+                defmt::assert!($pot > 0.98, "Assert failed, actual value: {:?}", $pot);
+                defmt::info!("OK");
+            };
+        }
 
-    //     defmt::info!("Turn all pots to their minimum value, then click the button");
+        defmt::info!("Turn all pots to their minimum value, then click the button");
+        sample_until_button_is_clicked(inputs);
+        defmt::assert!(
+            inputs.pots.pre_amp < 0.02
+                && inputs.pots.drive < 0.02
+                && inputs.pots.bias < 0.02
+                && inputs.pots.dry_wet < 0.02
+                && inputs.pots.wow_flut < 0.02
+                && inputs.pots.speed < 0.02
+                && inputs.pots.tone < 0.02
+                && inputs.pots.head.iter().all(|h| {
+                    h.position < 0.02 && h.volume < 0.02 && h.feedback < 0.02 && h.pan < 0.02
+                })
+        );
+        defmt::info!("OK");
+
+        assert_pot_is_up!("Pre-amp", inputs.pots.pre_amp);
+        assert_pot_is_up!("Drive", inputs.pots.drive);
+        assert_pot_is_up!("Bias", inputs.pots.bias);
+        assert_pot_is_up!("Dry/Wet", inputs.pots.dry_wet);
+        assert_pot_is_up!("Wow/Flut", inputs.pots.wow_flut);
+        assert_pot_is_up!("Speed", inputs.pots.speed);
+        assert_pot_is_up!("Tone", inputs.pots.tone);
+        for i in 0..4 {
+            defmt::info!("For head {}", i);
+            assert_pot_is_up!("Position", inputs.pots.head[i].position);
+            assert_pot_is_up!("Volume", inputs.pots.head[i].volume);
+            assert_pot_is_up!("Feedback", inputs.pots.head[i].feedback);
+            assert_pot_is_up!("Pan", inputs.pots.head[i].pan);
+        }
+    }
+
+    // #[test]
+    // fn all_switches_work(inputs: &mut Inputs) {
+    //     defmt::info!("Set all switches down, then click the button");
+
     //     sample_until_button_is_clicked(inputs);
-    //     defmt::assert!(
-    //         inputs.pots.pre_amp < 0.01
-    //             && inputs.pots.drive < 0.01
-    //             && inputs.pots.bias < 0.01
-    //             && inputs.pots.dry_wet < 0.01
-    //             && inputs.pots.wow_flut < 0.01
-    //             && inputs.pots.speed < 0.01
-    //             && inputs.pots.tone < 0.01
-    //             && inputs.pots.head.iter().all(|h| {
-    //                 h.position < 0.01 && h.volume < 0.01 && h.feedback < 0.01 && h.pan < 0.01
-    //             })
-    //     );
+    //     for i in 0..10 {
+    //         defmt::assert!(
+    //             !inputs.switches.switch[i].value,
+    //             "Switch {:?} is not down",
+    //             i + 1,
+    //         );
+    //     }
     //     defmt::info!("OK");
 
-    //     assert_pot_is_up!("Pre-amp", inputs.pots.pre_amp);
-    //     assert_pot_is_up!("Drive", inputs.pots.drive);
-    //     assert_pot_is_up!("Bias", inputs.pots.bias);
-    //     assert_pot_is_up!("Dry/Wet", inputs.pots.dry_wet);
-    //     assert_pot_is_up!("Wow/Flut", inputs.pots.wow_flut);
-    //     assert_pot_is_up!("Speed", inputs.pots.speed);
-    //     assert_pot_is_up!("Tone", inputs.pots.tone);
-    //     for i in 0..4 {
-    //         defmt::info!("For head {}", i);
-    //         assert_pot_is_up!("Position", inputs.pots.head[i].position);
-    //         assert_pot_is_up!("Volume", inputs.pots.head[i].volume);
-    //         assert_pot_is_up!("Feedback", inputs.pots.head[i].feedback);
-    //         assert_pot_is_up!("Pan", inputs.pots.head[i].pan);
+    //     defmt::info!("Set switches up, from left to right, click the button after each");
+    //     for i in 0..10 {
+    //         sample_until_button_is_clicked(inputs);
+    //         defmt::assert!(inputs.switches.switch[i].value);
+    //         defmt::info!("OK");
     //     }
     // }
 
-    #[test]
-    fn all_switches_work(inputs: &mut Inputs) {
-        defmt::info!("Set all switches down, then click the button");
-
-        sample_until_button_is_clicked(inputs);
-        for i in 0..10 {
-            defmt::assert!(
-                !inputs.switches.switch[i].value,
-                "Switch {:?} is not down",
-                i + 1,
-            );
-        }
-        defmt::info!("OK");
-
-        defmt::info!("Set switches up, from left to right, click the button after each");
-        for i in 0..10 {
-            sample_until_button_is_clicked(inputs);
-            defmt::assert!(inputs.switches.switch[i].value);
-            defmt::info!("OK");
-        }
-    }
-
-    #[test]
-    fn button_detects_click(inputs: &mut Inputs) {
-        defmt::info!("Click the button");
-        sample_until_button_is_clicked(inputs);
-        defmt::info!("OK");
-    }
+    // #[test]
+    // fn button_detects_click(inputs: &mut Inputs) {
+    //     defmt::info!("Click the button");
+    //     sample_until_button_is_clicked(inputs);
+    //     defmt::info!("OK");
+    // }
 
     // #[test]
     // fn cv_inputs_get_detected_and_properly_scale(inputs: &mut Inputs) {
