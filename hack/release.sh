fdd35619406980756094cbc28efb2db2d81fb11e
Petr Horacek <hrck@protonmail.com>
2023-01-03 20:44:34 +0100

    Cover firmware in version bumps
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 version=${1}
 
 sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
+sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" firmware/Cargo.toml
 sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" control/Cargo.toml
 sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" dsp/Cargo.toml
 sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" benches/Cargo.toml
