41c2720d72cf15873c02430230b119b8dd154ce3
Petr Horacek <hrck@protonmail.com>
2023-01-04 22:21:46 +0100

    Cleanup compressor code
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- dsp/src/compressor.rs
+++ dsp/src/compressor.rs
@@ -2,12 +2,15 @@
 //!
 //! Based on the README of <https://github.com/p-hlp/CTAGDRC>.
 
-// TODO: For stereo, analyzing max on both sides, applying equally
-
 use libm::{expf, fabsf, log10f, powf};
 
 const ATTACK_IN_SECONDS: f32 = 0.01;
 const RELEASE_IN_SECONDS: f32 = 0.14;
+const TRESHOLD: f32 = -12.0; // This is 0.5 amplitude
+const RATIO: f32 = 16.0;
+const SLOPE: f32 = 1.0 / RATIO - 1.0;
+const KNEE: f32 = 6.0;
+const KNEE_HALF: f32 = KNEE / 2.0;
 
 #[derive(Default, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
@@ -17,13 +20,6 @@ pub struct Compressor {
     alpha_release: f32,
 }
 
-// TODO:
-// - Convert to maximum sizes of amplitudes from both channels
-// - Convert to dbs
-// - Apply knee
-// - Apply smoothing (envelope)
-// - Convert back to linear
-// - Multiply the original signal by this
 impl Compressor {
     pub fn new(sample_rate: f32) -> Self {
         Self {
@@ -34,44 +30,30 @@ impl Compressor {
     }
 
     pub fn process(&mut self, buffer_left: &mut [f32], buffer_right: &mut [f32]) {
-        // TODO: Try optimizing this by processing each stage in a buffer
-
         for (l, r) in buffer_left.iter_mut().zip(buffer_right) {
             let max = f32::max(fabsf(*l), fabsf(*r));
             let level = f32::max(max, 1.0e-6);
             let level_in_decibels = 20.0 * log10f(level);
 
-            // TODO: Tweak this to keep good headroom
-            // NOTE: This is 0.5 amplitude
-            let treshold = -12.0;
-            // TODO: Tweak ratio to make sure loud feedback can fit easily
-            let ratio = 16.0;
-            let slope = 1.0 / ratio - 1.0;
-            let knee = 6.0;
-            let knee_half = knee / 2.0;
-
-            let overshoot = level_in_decibels - treshold;
-            let compressed = if overshoot < -knee_half {
+            let overshoot = level_in_decibels - TRESHOLD;
+            let compression = if overshoot < -KNEE_HALF {
                 0.0
-            } else if overshoot < knee_half {
-                0.5 * slope * ((overshoot + knee_half) * (overshoot + knee_half)) / knee
+            } else if overshoot < KNEE_HALF {
+                0.5 * SLOPE * ((overshoot + KNEE_HALF) * (overshoot + KNEE_HALF)) / KNEE
             } else {
-                slope * overshoot
+                SLOPE * overshoot
             };
 
-            self.n1 = if compressed < self.n1 {
-                self.alpha_attack * self.n1 + (1.0 - self.alpha_attack) * compressed
+            let filtered_compression = if compression < self.n1 {
+                self.alpha_attack * self.n1 + (1.0 - self.alpha_attack) * compression
             } else {
-                self.alpha_release * self.n1 + (1.0 - self.alpha_release) * compressed
+                self.alpha_release * self.n1 + (1.0 - self.alpha_release) * compression
             };
-            let filtered = self.n1;
+            self.n1 = filtered_compression;
+            let filtered_compression_linear = powf(10.0, filtered_compression / 20.0);
 
-            // TODO: Consider applying makeup
-
-            let level = powf(10.0, filtered / 20.0);
-
-            *l *= level;
-            *r *= level;
+            *l *= filtered_compression_linear;
+            *r *= filtered_compression_linear;
         }
     }
 }
