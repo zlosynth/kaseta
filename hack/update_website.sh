6afa109b0ddfb79ad719a19bc25854b9ce8705a1
Petr Horacek <hrck@protonmail.com>
2023-06-22 23:32:01 +0200

    Add script updating website with manuals
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

---
+++ hack/update_website.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+makers manual
+
+cp manual/user/manual.pdf ../zlosynth.github.io/docs/kaseta-user-manual.pdf
+cp manual/build/manual.pdf ../zlosynth.github.io/docs/kaseta-build-manual.pdf
