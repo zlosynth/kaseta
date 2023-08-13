b3dea42c3e316bb775e1967b28b0c965e0cbff5f
Petr Horacek <petr@zlosynth.com>
2023-08-13 23:23:12 +0200

    Bump storage token
    Signed-off-by: Petr Horacek <petr@zlosynth.com>

--- control/src/save.rs
+++ control/src/save.rs
@@ -29,7 +29,7 @@ impl Save {
 }
 
 // This constant is used to invalidate data when needed
-const TOKEN: u16 = 2;
+const TOKEN: u16 = 3;
 const CRC: Crc<u16> = Crc::<u16>::new(&CRC_16_USB);
 pub struct InvalidData;
 
